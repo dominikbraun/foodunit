4b5fcb85972fe970fd1b5e607a6654509037b40a
Dominik Braun <36575275+dominikbraun@users.noreply.github.com>
2019-02-22 23:51:43 +0100

    Add files via upload

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,5 +1,23 @@
 <template>
-    <div></div>
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="bg-white shadow-sm rounded-lg" style="width: 24rem;">
+            <div class="w-100">
+                <div class="bg-danger my-5 mx-auto text-center text-white text-theme h1 p-3 overflow-hidden rounded" style="width: 8rem;">
+                    Food&nbsp;<br />
+                    Unit&nbsp;
+                </div>
+            </div>
+            <form class="col-12">
+                <div class="form-group">
+                    <label for="formGroupExampleInput">Example label</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+                </div>
+                <button class="btn btn-success">
+                    dere
+                </button>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
