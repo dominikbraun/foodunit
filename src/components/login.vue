da44b0b552db95a0ef9048fc74eb3346611f9cbd
Dominik Braun <36575275+dominikbraun@users.noreply.github.com>
2019-02-24 19:20:14 +0100

    Add files via upload

--- src/components/login.vue
+++ src/components/login.vue
@@ -2,23 +2,23 @@
     <div class="row h-100">
         <div class="col-12 col-sm-4 h-100 d-none d-sm-block"></div>
         <div class="col-12 col-sm-4 h-100 justify-content-center align-items-center d-flex">
-            <div class="bg-white text-white p-5 rounded-lg shadow-lg">
-                <div class="my-3">
-                    <div class="bg-danger mx-auto text-center text-theme h1 p-3 rounded shadow mb-0" style="width: 8rem;">
+            <div class="bg-white p-5 rounded-lg shadow-lg w-100">
+                <div class="my-4">
+                    <div class="bg-danger mx-auto text-center text-white text-theme h1 p-3 rounded shadow mb-0" style="width: 8rem;">
                         Food&nbsp;<br />
                         Unit&nbsp;
                     </div>
                 </div>
-                <div class="my-5">
+                <div class="py-5">
                     <form autocomplete="off">
-                        <div class="form-group">
-                            <input type="text" class="form-control border-0 bg-light rounded shadow-sm text-md px-3 py-4" id="user-email" placeholder="Ihre E-Mail-Adresse" />
+                        <div class="form-group mb-0">
+                            <input type="text" class="form-control border-0 bg-light rounded shadow-sm text-md px-3 py-4" id="user-email" placeholder="Ihre E-Mail-Adresse" spellcheck="false" />
                         </div>
                     </form>
                 </div>
-                <div class="mt-3 text-center">
-                    <button class="btn btn-success border-thick rounded text-bold text-sm px-4 py-2 mx-auto shadow mb-3">
-                        LOGIN ANFORDERN
+                <div class="my-4 text-center">
+                    <button class="btn btn-success border-thick border-0 rounded text-bold text-uppercase text-sm px-4 py-3 mx-auto shadow">
+                        Login anfordern
                     </button>
                 </div>
             </div>
