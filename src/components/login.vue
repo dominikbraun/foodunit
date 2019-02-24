7dd5e9fa745cec95524491ad60c5d7a3814c9946
Dominik Braun <36575275+dominikbraun@users.noreply.github.com>
2019-02-24 18:46:58 +0100

    Add files via upload

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,27 +1,29 @@
 <template>
-    <div class="row h-100 justify-content-center align-items-center">
-        <div class="bg-white-tr rounded-xl p-0 p-sm-3">
-            <div class="bg-white shadow-lg rounded-lg" style="width: 21rem;">
-                <div class="pt-5 pb-5">
-                    <div class="bg-danger mx-auto text-center text-white text-theme h1 p-3 overflow-hidden rounded shadow-sm mt-3"
-                         style="width: 8rem;">
-                        Food&nbsp;<br/>
+    <div class="row h-100">
+        <div class="col-12 col-sm-4 h-100 d-none d-sm-block"></div>
+        <div class="col-12 col-sm-4 h-100 justify-content-center align-items-center d-flex">
+            <div class="bg-white text-white p-5 rounded-lg shadow-lg">
+                <div class="my-3">
+                    <div class="bg-danger mx-auto text-center text-theme h1 p-3 rounded shadow mb-0" style="width: 8rem;">
+                        Food&nbsp;<br />
                         Unit&nbsp;
                     </div>
                 </div>
-                <form class="col-12">
-                    <div class="form-group w-75 mx-auto my-0">
-                        <input type="text" class="form-control border-0 shadow-sm bg-light text-md px-3 py-4 my-2"
-                               id="formGroupExampleInput" placeholder="E-Mail-Adresse">
-                    </div>
-                    <div class="form-group mx-auto text-center my-0 pt-5 pb-5">
-                        <button class="btn btn-success border-thick rounded text-bold text-sm px-4 py-2 mx-auto shadow-sm mb-3">
-                            LOGIN ANFORDERN
-                        </button>
-                    </div>
-                </form>
+                <div class="my-5">
+                    <form autocomplete="off">
+                        <div class="form-group">
+                            <input type="text" class="form-control border-0 bg-light rounded shadow-sm text-md px-3 py-4" id="user-email" placeholder="Ihre E-Mail-Adresse" />
+                        </div>
+                    </form>
+                </div>
+                <div class="mt-3 text-center">
+                    <button class="btn btn-success border-thick rounded text-bold text-sm px-4 py-2 mx-auto shadow mb-3">
+                        LOGIN ANFORDERN
+                    </button>
+                </div>
             </div>
         </div>
+        <div class="col-12 col-sm-4 h-100 d-none d-sm-block"></div>
     </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'FoodUnit',
+      name: '',
       component: main
     },
     {
