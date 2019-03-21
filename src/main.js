c67c7cb13d54e2c988c9299686accc70d9236ded
Dominik Braun <braun@sternentstehung.de>
2019-03-21 22:28:57 +0100

    extended HTML and added test data

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-let validSession = false;
+let validSession = true;
+
+let supplier = {
+    name: "McDonald's Straubing",
+    st: "Ittlingerstr. 119",
+    ph: "+49 9424 3394948",
+    opened: "07:00 - 01:00",
+    cats: [
+        {
+            name: "Hamburger & Co.",
+            dishes: [
+                { name: "Hamburger Royal Käse", price: 4.19 },
+                { name: "Hamburger Royal TS", price: 4.19 },
+                { name: "McRib", price: 3.99 },
+                { name: "Pommes kl.", price: 1.79 },
+            ]
+        }
+    ]
+};
 
 new Vue({
     el: '#app',
     data: {
-        loggedIn: validSession
+        loggedIn: validSession,
+        supplier: supplier,
     }
 });
