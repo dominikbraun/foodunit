04ec6abfd8923dcd3600c7b2e020256e9a5e0bce
Dominik Braun <braun@sternentstehung.de>
2019-03-22 21:42:47 +0100

    added test data and changed styles

--- src/main.js
+++ src/main.js
@@ -10,20 +10,22 @@ let supplier = {
             name: "Hamburger & Co.",
             img: "hamburgers.jpg",
             dishes: [
-                { name: "Hamburger Royal Käse", price: 4.19 },
-                { name: "Hamburger Royal TS", price: 4.19 },
-                { name: "McRib", price: 3.99 },
-                { name: "Pommes kl.", price: 1.79 },
+                { name: "Hamburger Royal Käse", desc: "Saftiges Rindfleisch mit ausgewählten Zutaten", price: 4.19 },
+                { name: "Hamburger Royal TS", desc: "Saftiges Rindfleisch mit ausgewählten Zutaten", price: 4.19 },
+                { name: "McRib", desc: "Saftiges Rindfleisch mit ausgewählten Zutaten", price: 3.99 },
+                { name: "Pommes kl.", desc: "Saftiges Rindfleisch mit ausgewählten Zutaten mit Rohkostsalat und gebratenem Speck mit frischem Spiegelei", price: 1.79 },
+                { name: "McRib", desc: "Saftiges Rindfleisch mit ausgewählten Zutaten", price: 3.99 },
+                { name: "Hamburger Royal TS", desc: "Saftiges Rindfleisch mit ausgewählten Zutaten", price: 4.19 },
             ]
         },
         {
             name: "The Signature Collection",
             img: "siganture-collection.jpg",
             dishes: [
-                { name: "Hamburger Royal Käse", price: 4.19 },
-                { name: "Hamburger Royal TS", price: 4.19 },
-                { name: "McRib", price: 3.99 },
-                { name: "Pommes kl.", price: 1.79 },
+                { name: "Hamburger Royal Käse", desc: "Saftiges Rindfleisch mit ausgewählten Zutaten", price: 4.19 },
+                { name: "Hamburger Royal TS", desc: "Saftiges Rindfleisch mit ausgewählten Zutaten", price: 4.19 },
+                { name: "McRib", desc: "Saftiges Rindfleisch mit ausgewählten Zutaten", price: 3.99 },
+                { name: "Pommes kl.", desc: "Saftiges Rindfleisch mit ausgewählten Zutaten", price: 1.79 },
             ]
         }
     ]
