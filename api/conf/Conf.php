a01059d9e57f1b448548596491e2277c79959e66
Dominik Braun <braun@sternentstehung.de>
2019-05-16 20:06:10 +0200

    Config-Handling implementiert

---
+++ api/conf/Conf.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace foodunit\conf;
+
+class Conf
+{
+    private static $conf;
+
+    public static function init()
+    {
+        self::$conf = parse_ini_file('conf.ini');
+    }
+
+    public static function get(string $key)
+    {
+        if (!isset(self::$conf[$key])) {
+            return false;
+        }
+        return self::$conf[$key];
+    }
+}
