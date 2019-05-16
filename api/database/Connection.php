4fc55bee91d79b98a34269bfe825a23b56fda0dd
Dominik Braun <braun@sternentstehung.de>
2019-05-16 20:54:08 +0200

    Methoden für Datenbankabfragen implementiert

--- api/database/Connection.php
+++ api/database/Connection.php
@@ -16,11 +16,44 @@ class Connection
     private $pdo;
 
     /**
+     * @var string
+     */
+    private $okCode;
+
+    /**
      * Connection constructor.
      */
     public function __construct()
     {
         $con = Conf::get('db_con_str');
+
         $this->pdo = new \PDO($con, Conf::get('user'), Conf::get('pass'));
+        $this->okCode = '00000';
+    }
+
+    /**
+     * @param string $sql
+     * @param array $bindings
+     * @return array
+     */
+    public function query(string $sql, array $bindings = [])
+    {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($bindings);
+
+        return $stmt->fetchAll();
+    }
+
+    /**
+     * @param string $sql
+     * @param array $bindings
+     * @return bool
+     */
+    public function exec(string $sql, array $bindings = [])
+    {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($bindings);
+
+        return $stmt->errorCode() === $this->okCode;
     }
 }
