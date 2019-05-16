5b9d76c985e4f5bc16842bd96d78819ba6b0fc82
Dominik Braun <braun@sternentstehung.de>
2019-05-16 20:07:56 +0200

    Annotationen hinzugefuegt

--- api/conf/Conf.php
+++ api/conf/Conf.php
@@ -2,15 +2,29 @@
 
 namespace foodunit\conf;
 
+/**
+ * Class Conf
+ * @package foodunit\conf
+ */
 class Conf
 {
+    /**
+     * @var array $conf
+     */
     private static $conf;
 
+    /**
+     * Conf initializer.
+     */
     public static function init()
     {
         self::$conf = parse_ini_file('conf.ini');
     }
 
+    /**
+     * @param string $key
+     * @return bool|mixed
+     */
     public static function get(string $key)
     {
         if (!isset(self::$conf[$key])) {
