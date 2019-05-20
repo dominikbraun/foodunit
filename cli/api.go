cad482b13b4d00553b45840d448c3afa9432921e
Dominik Braun <braun@sternentstehung.de>
2019-05-20 20:24:37 +0200

    Methoden zum Senden von Requests erstellt

--- cli/api.go
+++ cli/api.go
@@ -1,12 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"io"
+	"net/http"
+)
 
 type Api struct {
 	url    string
 	client *http.Client
 }
 
-func (a *Api) req(endpoint string) *http.Response {
-	return &http.Response{}
+func (a *Api) req(endpoint string) (*http.Response, error) {
+	if a.client == nil {
+		return nil, errors.New("API client not set")
+	}
+	reqUrl := fmt.Sprintf("%s%s", a.url, endpoint)
+	req, err := http.NewRequest("GET", reqUrl, nil)
+
+	if err != nil {
+		return nil, err
+	}
+	return a.client.Do(req)
+}
+
+func (a *Api) reqBody(res *http.Response) io.ReadCloser {
+	return res.Body
 }
