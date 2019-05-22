4fad58049f9a2640a4fd3f899da46f98074b9a17
Dominik Braun <braun@sternentstehung.de>
2019-05-22 19:33:20 +0200

    Handling-Funktionen neu strukturiert

---
+++ cli/handler.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/spf13/cobra"
+
+var rootHandler = func(cmd *cobra.Command, args []string) {
+	//
+}
+
+var offerHandler = func(cmd *cobra.Command, args []string) {
+	//
+}
