7b963c07287760077c1daecc3fb2f9ce26cb094d
Dominik Braun <braun@sternentstehung.de>
2019-05-28 23:00:35 +0200

    Neue Rendering-Funktionen implementiert

--- src/foodunit--main.js
+++ src/foodunit--main.js
@@ -1,30 +1,31 @@
-let cats = []
-let catIndex = 0
+let menu = []
+let page = 0
 let total = 0
 
 $(function () {
     $('#prev').on('click', function () {
-        if (catIndex > 0) {
-            renderMenuPage(--catIndex)
+        if (page > 0) {
+            renderMenuPage(--page)
         }
     })
     $('#next').on('click', function () {
-        if (catIndex < cats.length - 1) {
-            renderMenuPage(++catIndex)
+        if (page < menu.length - 1) {
+            renderMenuPage(++page)
         }
     })
-    chooseOfferAndRenderPage()
+    buildPage()
 })
 
-function chooseOfferAndRenderPage() {
+function buildPage() {
     $.ajax({
         url: 'api/offers',
         type: 'get',
         success: function (res) {
             let offers = JSON.parse(res)
             let supplierId = offers[0].supplier_id
+
             buildSupplier(supplierId)
-            renderCart(offers[0].id)
+            buildCart(offers[0].id)
         }
     })
 }
@@ -35,7 +36,10 @@ function buildSupplier(supplierId) {
         type: 'get',
         success: function (res) {
             let s = JSON.parse(res)
+            menu = s.menu
             renderSupplierInfo(s.name, s.address, s.mon, s.phone)
+            renderCats(menu.map(c => c.name))
+            renderMenuPage(page)
         }
     })
 }
@@ -50,7 +54,7 @@ function renderSupplierInfo(name, address, opened, phone) {
 
 function renderCats(cats) {
     for (let i = 0; i < cats.length; i++) {
-        let html = '<a class="text-dark mx-2" href="">' + cats[i].name + '</a>'
+        let html = '<a class="text-dark mx-2" href="">' + cats[i] + '</a>'
 
         if (i < cats.length - 1) {
             html += '/'
@@ -59,41 +63,20 @@ function renderCats(cats) {
     }
 }
 
-function renderMenu(supplierId) {
-    $.ajax({
-        url: 'api/menu/' + supplierId,
-        type: 'get',
-        success: function (res) {
-            cats = JSON.parse(res)
-
-            for (let i = 0; i < cats.length; i++) {
-                let html = '<a class="text-dark mx-2" href="">' + cats[i].name + '</a>'
-
-                if (i < cats.length - 1) {
-                    html += '/'
-                }
-                $('#cats').append(html)
-            }
-
-            renderMenuPage(catIndex)
-        }
-    })
-}
-
 function renderMenuPage(i) {
-    $('#cat').text(cats[i].name)
+    $('#cat').text(menu[i].name)
 
-    $('#bg-container').css('background-image', 'static/img/' + cats[i].img)
+    $('#bg-container').css('background-image', 'static/img/' + menu[i].img)
 
     $('#dishes').empty()
 
-    for (let d of cats[i].dishes) {
+    for (let d of menu[i].dishes) {
         let html = '<div class="col-12 col-xl-6 px-5 py-3 add-item"><div class="row"><div class="col-12 col-lg-9"><span class="text-md text-very-strong">' + d.name + '</span><br /><span class="text-xs"><i>' + d.desc + '</i></span></div><div class="col-12 col-lg-3 text-right">+ <span class="text-white text-strong price" href="">' + Number.parseFloat(d.price).toFixed(2) + ' &euro;</span></div></div></div>'
         $('#dishes').append(html)
     }
 }
 
-function renderCart(offerId) {
+function buildCart(offerId) {
     $.ajax({
         url: 'api/user-order/' + offerId,
         type: 'get',
