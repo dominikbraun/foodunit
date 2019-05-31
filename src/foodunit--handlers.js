cd296e94c934577dac457c6e7545e07d60a3739b
Dominik Braun <braun@sternentstehung.de>
2019-05-31 12:59:13 +0200

    SSO-Login implementiert

--- src/foodunit--handlers.js
+++ src/foodunit--handlers.js
@@ -1,9 +1,41 @@
-const save = '#save_cart'
+const loginBtn = '#login'
+const emailInp = '#email'
+const saveBtn = '#save-cart'
 
 $(register)
 
 function register() {
-    $(save).on('click', saveCart)
+    $(loginBtn).on('click', function (e) {
+        e.preventDefault()
+        login()
+    })
+    $(saveBtn).on('click', saveCart)
+}
+
+function login() {
+    $(loginBtn).attr('disabled', true)
+    $(loginBtn).html('<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>')
+
+    let email = $(emailInp).val()
+    let endpoint = '/sso/' + email
+
+    req(endpoint, function (res) {
+        let ok = JSON.parse(res) === true
+        if (ok) {
+            $(loginBtn).html('E-Mail gesendet')
+        } else {
+            $(loginBtn).html('Es ist ein Fehler aufgetreten.')
+        }
+    })
 }
 
 function saveCart() {}
+
+function req(endpoint, success) {
+    console.log(endpoint)
+    $.ajax({
+        url: 'api' + endpoint,
+        type: 'get',
+        success: success
+    })
+}
