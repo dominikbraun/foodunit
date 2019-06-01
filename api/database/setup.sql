9c8ded2ac28cad2f3beadbbc8f21d21de51247dd
Dominik Braun <braun@sternentstehung.de>
2019-06-01 21:25:12 +0200

    Setup-Datei aktualisiert

--- api/database/setup.sql
+++ api/database/setup.sql
@@ -1,6 +1,11 @@
 -- phpMyAdmin SQL Dump
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 01. Jun 2019 um 21:24
+-- Server-Version: 10.1.36-MariaDB
+-- PHP-Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,9 +30,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cats` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` text NOT NULL,
-  `img` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `name` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
+  `img` mediumtext COLLATE utf8mb4_german2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
 
 -- --------------------------------------------------------
 
@@ -39,10 +44,10 @@ CREATE TABLE `dishes` (
   `id` int(10) UNSIGNED NOT NULL,
   `supplier_id` int(10) UNSIGNED NOT NULL,
   `cat_id` int(10) UNSIGNED NOT NULL,
-  `name` text NOT NULL,
-  `description` text NOT NULL,
+  `name` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
+  `description` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
   `price` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
 
 -- --------------------------------------------------------
 
@@ -55,7 +60,7 @@ CREATE TABLE `offers` (
   `supplier_id` int(10) UNSIGNED NOT NULL,
   `start` datetime DEFAULT NULL,
   `end` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
 
 -- --------------------------------------------------------
 
@@ -68,7 +73,7 @@ CREATE TABLE `orders` (
   `offer_id` int(10) UNSIGNED NOT NULL,
   `dish_id` int(10) UNSIGNED NOT NULL,
   `session_id` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
 
 -- --------------------------------------------------------
 
@@ -80,8 +85,8 @@ CREATE TABLE `remarks` (
   `id` int(10) UNSIGNED NOT NULL,
   `offer_id` int(10) UNSIGNED NOT NULL,
   `session_id` int(10) UNSIGNED NOT NULL,
-  `remark` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `remark` mediumtext COLLATE utf8mb4_german2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
 
 -- --------------------------------------------------------
 
@@ -91,12 +96,12 @@ CREATE TABLE `remarks` (
 
 CREATE TABLE `sessions` (
   `id` int(10) UNSIGNED NOT NULL,
-  `_key` text NOT NULL,
-  `email` text NOT NULL,
+  `_key` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
+  `email` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
   `valid` tinyint(1) NOT NULL DEFAULT '0',
-  `confirmation_token` text NOT NULL,
+  `confirmation_token` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
   `confirmed` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
 
 -- --------------------------------------------------------
 
@@ -106,15 +111,15 @@ CREATE TABLE `sessions` (
 
 CREATE TABLE `suppliers` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` text NOT NULL,
-  `address` text NOT NULL,
-  `phone` text NOT NULL,
-  `mon` text NOT NULL,
-  `tue` text NOT NULL,
-  `wed` text NOT NULL,
-  `thu` text NOT NULL,
-  `fri` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `name` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
+  `address` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
+  `phone` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
+  `mon` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
+  `tue` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
+  `wed` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
+  `thu` mediumtext COLLATE utf8mb4_german2_ci NOT NULL,
+  `fri` mediumtext COLLATE utf8mb4_german2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
 
 --
 -- Indizes der exportierten Tabellen
