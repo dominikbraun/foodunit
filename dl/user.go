af0d9510818948387ca65cb922d9297142ec1820
Dominik Braun <braun@sternentstehung.de>
2019-08-05 17:50:10 +0200

    created DL description for user

--- dl/user.go
+++ dl/user.go
@@ -1,1 +1,21 @@
 package dl
+
+// DL description for `User` and its ORM repository.
+
+// `User` represents a person which can login, create offers
+// or order food for themselves.
+type User struct {
+	ID   uint   `gorm:"primary_key"`
+	Mail string `gorm:"type:varchar(254);unique"`
+	Name string `gorm:"type:varchar(100)"`
+}
+
+// `UserRepository` provides methods for typical CRUD operations.
+// Its implementations are stored in the /gateways package.
+type UserRepository interface {
+	Create(u *User) error
+	Find(id uint) *User
+	FindByMail(mail string) *User
+	Update(u *User) error
+	Delete(u *User) error
+}
