6e1bbb313204a3943861fe0c31f607f7ec6d9dc2
Dominik Braun <braun@sternentstehung.de>
2019-08-09 23:00:39 +0200

    created functions for retrieving implementations

---
+++ core/load/repositories.go
@@ -0,0 +1,31 @@
+package load
+
+import "github.com/dominikbraun/foodunit/dl"
+
+func UserRepository() dl.UserRepository {
+	return nil
+}
+
+func SupplierRepository() dl.SupplierRepository {
+	return nil
+}
+
+func CategoryRepository() dl.CategoryRepository {
+	return nil
+}
+
+func DishRepository() dl.DishRepository {
+	return nil
+}
+
+func OfferRepository() dl.OfferRepository {
+	return nil
+}
+
+func OrderRepository() dl.OrderRepository {
+	return nil
+}
+
+func PositionRepository() dl.PositionRepository {
+	return nil
+}
