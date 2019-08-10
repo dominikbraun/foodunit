dded8007b6ebd8e53e07d9bb1e3aea0ee8bc441b
Dominik Braun <braun@sternentstehung.de>
2019-08-10 14:05:17 +0200

    created global sqlx DB

---
+++ gateways/sql/db.go
@@ -0,0 +1,28 @@
+package sql
+
+import (
+	"errors"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+)
+
+var instance *sqlx.DB = nil
+
+func Connect(driver, dbname, user, pass string) error {
+	dsn := fmt.Sprintf("user=%s dbname=%s sslmode=disable", user, dbname)
+
+	db, err := sqlx.Connect(driver, dsn)
+	if err != nil {
+		return err
+	}
+
+	instance = db
+	return nil
+}
+
+func Instance() (*sqlx.DB, error) {
+	if instance == nil {
+		return nil, errors.New("invalid database instance - call `sql.Connect` first")
+	}
+	return instance, nil
+}
