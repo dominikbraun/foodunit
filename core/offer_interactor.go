bd5cda8145915ba3fa2741e63f3447287081abe1
Dominik Braun <braun@sternentstehung.de>
2019-08-10 13:16:10 +0200

    implemented SaveOrder

--- core/offer_interactor.go
+++ core/offer_interactor.go
@@ -8,6 +8,10 @@ import (
 	"github.com/pkg/errors"
 )
 
+// `PositionMap` holds multiple positions in an user's order. The key
+// represents the `DishID`, while the value depicts the `Note`.
+type PositionMap map[uint64]string
+
 // `OfferInteractor` represents an interface to be used by the adapters
 // for triggering the core business logic and receiving the results.
 type OfferInteractor struct {
@@ -31,8 +35,33 @@ func NewOfferInteractor() (*OfferInteractor, error) {
 	}, nil
 }
 
+// `SaveOrder` persists a new order that contains the given positions.
+func (o *OfferInteractor) SaveOrder(userID, offerID uint64, pos PositionMap) error {
+	order := dl.Order{
+		UserID:  userID,
+		OfferID: offerID,
+	}
+
+	orderID, err := o.orders.Create(&order)
+	if err != nil {
+		return err
+	}
+
+	for id, note := range pos {
+		position := dl.Position{
+			ID:      0,
+			OrderID: orderID,
+			DishID:  id,
+			Note:    note,
+		}
+		// ToDo: The error value of `Create` has to be handled.
+		_, _ = o.positions.Create(&position)
+	}
+	return nil
+}
+
 // `GetOrders` returns all saved orders for a given offer.
-func (o *OfferInteractor) GetOrders(offerID uint64) []*dl.Order {
-	orders := o.orders.FindByOfferID(offerID)
-	return orders
+func (o *OfferInteractor) GetOrders(offerID uint64) ([]*dl.Order, error) {
+	orders, err := o.orders.FindByOfferID(offerID)
+	return orders, err
 }
