9094ff1c0e9eee1832ac6e3d93771367395ef1ef
Dominik Braun <braun@sternentstehung.de>
2019-08-10 13:48:41 +0200

    scaffolded method implementations of DL repositories

--- gateways/sql/offer_repository.go
+++ gateways/sql/offer_repository.go
@@ -1,1 +1,105 @@
+// package `sql` provides repository implementations as SQL gateways.
 package sql
+
+import (
+	"github.com/dominikbraun/foodunit/dl"
+)
+
+type (
+	// `OfferRepository` holds methods of `dl.OfferRepository`.
+	OfferRepository struct{}
+	// `OrderRepository` holds methods of `dl.OrderRepository`.
+	OrderRepository struct{}
+	// `PositionRepository` holds methods of `dl.PositionRepository`.
+	PositionRepository struct{}
+)
+
+// `Migrate` implements `dl.OfferRepository.`.
+func (o OfferRepository) Migrate() error {
+	panic("implement me")
+}
+
+// `Create` implements `dl.OfferRepository.Create`.
+func (o OfferRepository) Create(offer *dl.Offer) (uint64, error) {
+	panic("implement me")
+}
+
+// `Find` implements `dl.OfferRepository.Find`.
+func (o OfferRepository) Find(id uint64) *dl.Offer {
+	panic("implement me")
+}
+
+// `FindAllActive` implements `dl.OfferRepository.FindAllActive`.
+func (o OfferRepository) FindAllActive() []*dl.Offer {
+	panic("implement me")
+}
+
+// `Update` implements `dl.OfferRepository.Update`.
+func (o OfferRepository) Update(offer *dl.Offer) error {
+	panic("implement me")
+}
+
+// `Delete` implements `dl.OfferRepository.Delete`.
+func (o OfferRepository) Delete(offer *dl.Offer) error {
+	panic("implement me")
+}
+
+// `Migrate` implements `dl.OrderRepository.Migrate`.
+func (o OrderRepository) Migrate() error {
+	panic("implement me")
+}
+
+// `Create` implements `dl.OrderRepository.Create`.
+func (o OrderRepository) Create(order *dl.Order) (uint64, error) {
+	panic("implement me")
+}
+
+// `Find` implements `dl.OrderRepository.Find`.
+func (o OrderRepository) Find(id uint64) (*dl.Order, error) {
+	panic("implement me")
+}
+
+// `FindByOfferID` implements `dl.OrderRepository.FindByOfferID`.
+func (o OrderRepository) FindByOfferID(offerID uint64) ([]*dl.Order, error) {
+	panic("implement me")
+}
+
+// `Update` implements `dl.OrderRepository.Update`.
+func (o OrderRepository) Update(order *dl.Order) error {
+	panic("implement me")
+}
+
+// `Delete` implements `dl.OrderRepository.Delete`.
+func (o OrderRepository) Delete(order *dl.Order) error {
+	panic("implement me")
+}
+
+// `Migrate` implements `dl.PositionRepository.Migrate`.
+func (p PositionRepository) Migrate() error {
+	panic("implement me")
+}
+
+// `Create` implements `dl.PositionRepository.Create`.
+func (p PositionRepository) Create(position *dl.Position) (uint64, error) {
+	panic("implement me")
+}
+
+// `Find` implements `dl.PositionRepository.Find`.
+func (p PositionRepository) Find(id uint64) (*dl.Position, error) {
+	panic("implement me")
+}
+
+// `FindByOrderID` implements `dl.PositionRepository.FindByOrderID`.
+func (p PositionRepository) FindByOrderID(orderID uint64) ([]*dl.Position, error) {
+	panic("implement me")
+}
+
+// `Update` implements `dl.PositionRepository.Update`.
+func (p PositionRepository) Update(position *dl.Position) error {
+	panic("implement me")
+}
+
+// `Delete` implements `dl.PositionRepository.Delete`.
+func (p PositionRepository) Delete(position *dl.Position) error {
+	panic("implement me")
+}
--- gateways/sql/supplier_repository.go
+++ gateways/sql/supplier_repository.go
@@ -1,1 +1,100 @@
+// package `sql` provides repository implementations as SQL gateways.
 package sql
+
+import (
+	"github.com/dominikbraun/foodunit/dl"
+)
+
+type (
+	// `SupplierRepository` holds methods of `dl.SupplierRepository`.
+	SupplierRepository struct{}
+	// `CategoryRepository` holds methods of `dl.CategoryRepository`.
+	CategoryRepository struct{}
+	// `DishRepository` holds methods of `dl.DishRepository`.
+	DishRepository struct{}
+)
+
+// `Migrate` implements `dl.SupplierRepository.Migrate`.
+func (s SupplierRepository) Migrate() error {
+	panic("implement me")
+}
+
+// `Create` implements `dl.SupplierRepository.Create`.
+func (s SupplierRepository) Create(supplier *dl.Supplier) (uint64, error) {
+	panic("implement me")
+}
+
+// `Find` implements `dl.SupplierRepository.Find`.
+func (s SupplierRepository) Find(id uint64) (*dl.Supplier, error) {
+	panic("implement me")
+}
+
+// `Update` implements `dl.SupplierRepository.Update`.
+func (s SupplierRepository) Update(supplier *dl.Supplier) error {
+	panic("implement me")
+}
+
+// `Delete` implements `dl.SupplierRepository.Delete`.
+func (s SupplierRepository) Delete(supplier *dl.Supplier) error {
+	panic("implement me")
+}
+
+// `Migrate` implements `dl.CategoryRepository.Migrate`.
+func (c CategoryRepository) Migrate() error {
+	panic("implement me")
+}
+
+// `Create` implements `dl.CategoryRepository.Create`.
+func (c CategoryRepository) Create(category *dl.Category) (uint64, error) {
+	panic("implement me")
+}
+
+// `Find` implements `dl.CategoryRepository.Find`.
+func (c CategoryRepository) Find(id uint64) (*dl.Category, error) {
+	panic("implement me")
+}
+
+// `FindBySupplierID` implements `dl.CategoryRepository.FindBySupplierID`.
+func (c CategoryRepository) FindBySupplierID(supplierID uint64) ([]*dl.Category, error) {
+	panic("implement me")
+}
+
+// `Update` implements `dl.CategoryRepository.Update`.
+func (c CategoryRepository) Update(category *dl.Category) error {
+	panic("implement me")
+}
+
+// `Delete` implements `dl.CategoryRepository.Delete`.
+func (c CategoryRepository) Delete(category *dl.Category) error {
+	panic("implement me")
+}
+
+// `Migrate` implements `dl.DishRepository.Migrate`.
+func (d DishRepository) Migrate() error {
+	panic("implement me")
+}
+
+// `Create` implements `dl.DishRepository.Create`.
+func (d DishRepository) Create(dish *dl.Dish) (uint64, error) {
+	panic("implement me")
+}
+
+// `Find` implements `dl.DishRepository.Find`.
+func (d DishRepository) Find(id uint64) (*dl.Dish, error) {
+	panic("implement me")
+}
+
+// `FindByCategoryID` implements `dl.DishRepository.FindByCategoryID`.
+func (d DishRepository) FindByCategoryID(categoryID uint64) ([]*dl.Dish, error) {
+	panic("implement me")
+}
+
+// `Update` implements `dl.DishRepository.Update`.
+func (d DishRepository) Update(dish *dl.Dish) error {
+	panic("implement me")
+}
+
+// `Delete` implements `dl.DishRepository.Delete`.
+func (d DishRepository) Delete(dish *dl.Dish) error {
+	panic("implement me")
+}
