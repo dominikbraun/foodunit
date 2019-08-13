536e6132fd0016da31b5fe0ba703c1bf413b16c1
Dominik Braun <braun@sternentstehung.de>
2019-08-13 22:26:37 +0200

    built offer routes

--- controllers/http/handlers.go
+++ controllers/http/handlers.go
@@ -1,1 +1,29 @@
+// Package http provides HTTP handlers, functioning as
+// interfaces between the API server and the use cases.
 package http
+
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+)
+
+// GetOffer renders a given offer.
+func GetOffer(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, "GetOffer")
+}
+
+// PutOffer updates a given offer.
+func PutOffer(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, "PutOffer")
+}
+
+// GetOrders renders all orders for the given offer.
+func GetOrders(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, "GetOrders")
+}
+
+// PutOrder updates an order associated with the current user.
+func PutOrder(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, "PutOrder")
+}
--- server/routes.go
+++ server/routes.go
@@ -1,1 +1,35 @@
+// Package server provides functions to control FoodUnit's HTTP Server.
 package server
+
+import (
+	"github.com/dominikbraun/foodunit/controllers/http"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+)
+
+// routes builds the API routes and returns the app's router.
+func routes() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(
+		middleware.Logger,
+		middleware.DefaultCompress,
+		middleware.RedirectSlashes,
+		middleware.Recoverer,
+		render.SetContentType(render.ContentTypeJSON),
+	)
+
+	r.Route("/offers", func(r chi.Router) {
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", http.GetOffer)
+			r.Put("/", http.PutOffer)
+
+			r.Route("/orders", func(r chi.Router) {
+				r.Get("/", http.GetOrders)
+				r.Put("/", http.PutOrder)
+			})
+		})
+	})
+
+	return r
+}
