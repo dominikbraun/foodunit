4324b7914cbc87579b8166c241c2b1a51dd0d0e7
Dominik Braun <braun@sternentstehung.de>
2019-08-14 18:23:20 +0200

    renamed file

--- controllers/http/offer_handlers.go
+++ controllers/http/offer_handlers.go
@@ -8,6 +8,11 @@ import (
 	"github.com/go-chi/render"
 )
 
+// PostOffer creates a new offer.
+func PostOffer(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, "PostOffer")
+}
+
 // GetOffer renders a given offer.
 func GetOffer(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, "GetOffer")
@@ -18,12 +23,26 @@ func PutOffer(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, "PutOffer")
 }
 
+// DeleteOffer deletes a given offer.
+func DeleteOffer(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, "DeleteOffer")
+}
+
 // GetOrders renders all orders for the given offer.
 func GetOrders(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, "GetOrders")
 }
 
+// PostOrder creates an order associated with the current user.
+func PostOrder(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, "PostOrder")
+}
+
 // PutOrder updates an order associated with the current user.
 func PutOrder(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, "PutOrder")
+}
+
+func F(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, "Needs to be implemented")
 }
