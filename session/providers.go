6e1c6e5c97ff7e5b805f4d820d0e31d81f564dfa
Dominik Braun <braun@sternentstehung.de>
2019-09-12 19:10:53 +0200

    created session.Manager interface

---
+++ session/providers.go
@@ -0,0 +1,24 @@
+// Copyright 2019 The FoodUnit Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package session provides utility types and functions for session management.
+package session
+
+import scs2 "github.com/alexedwards/scs/v2"
+
+// ProvideManager returns an implementation of Manager.
+func ProvideManager() Manager {
+	scs := scs2.New()
+	return scs
+}
