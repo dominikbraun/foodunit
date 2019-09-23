b09f1f150731dc02180116bf7f9e82193bfb84d6
Dominik Braun <braun@sternentstehung.de>
2019-09-23 18:00:29 +0200

    created migration manager

---
+++ migration/manager.go
@@ -0,0 +1,71 @@
+// Copyright 2019 The FoodUnit Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package migration provides tools for performing data migrations.
+package migration
+
+import (
+	"github.com/dominikbraun/foodunit/storage"
+	"github.com/dominikbraun/foodunit/storage/maria"
+	"github.com/jmoiron/sqlx"
+	"log"
+)
+
+type Config struct {
+	Driver string `json:"driver"`
+	DSN    string `json:"dsn"`
+}
+
+type Manger struct {
+	db       *sqlx.DB
+	entities []storage.Entity
+}
+
+func NewManager(config *Config) (*Manger, error) {
+	m := Manger{}
+	var err error
+
+	m.db, err = sqlx.Open(config.Driver, config.DSN)
+	m.entities = []storage.Entity{
+		maria.NewRestaurant(m.db),
+		maria.NewCategory(m.db),
+		maria.NewDish(m.db),
+		maria.NewCharacteristic(m.db),
+		maria.NewVariant(m.db),
+		maria.NewUser(m.db),
+		maria.NewOffer(m.db),
+		maria.NewOrder(m.db),
+		maria.NewPosition(m.db),
+	}
+
+	return &m, err
+}
+
+func (m *Manger) RunPreparation(dropSchema bool) {
+	var err error
+
+	if dropSchema {
+		for _, s := range m.entities {
+			if err = s.Drop(); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	for _, s := range m.entities {
+		if err = s.Create(); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
