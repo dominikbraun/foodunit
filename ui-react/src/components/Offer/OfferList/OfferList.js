753b907b8baae08fa6024107911bd822375601a9
aligator <aligator@suncraft-server.de>
2019-10-16 23:17:54 +0200

    init react client

---
+++ ui-react/src/components/Offer/OfferList/OfferList.js
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2019 The FoodUnit Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+
+// ToDo: combine both functions in one as the have similar content
+export function OfferListCurrent(props) {
+    return <div className="mx-0 mx-xl-5 my-4 px-5 py-3 bg-white border rounded-0">
+        <h6 className="text-dark text-strong px-0 py-3">Aktuelle Angebote</h6>
+
+        <table className="table table-responsive-xl mb-0 text-center">
+            <tr>
+                <td className="align-middle pl-0 pr-3 py-4">
+                    <div className="text-hand text-lg bg-gradient rounded-0 text-dark text-center px-1 py-2">Pizzeria Venezia</div>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Bestellung m&ouml;glich:</p>
+                    <p className="text-md text-strong mb-0">Di, 11:15 &ndash; 11:45 Uhr</p>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Angebot erstellt von:</p>
+                    <p className="text-md text-strong mb-0">Dominik Braun</p>
+                </td>
+                <td className="align-middle px-0 py-3 text-center">
+                    <a href="restaurant.html" className="btn btn-light rounded-pill text-sm">Angebot ausw&auml;hlen</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td className="align-middle pl-0 pr-3 py-4">
+                    <div className="text-hand text-lg bg-gradient rounded-0 text-dark text-center px-1 py-2">Imbiss Media</div>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Bestellung m&ouml;glich:</p>
+                    <p className="text-md text-strong mb-0">Di, 11:15 &ndash; 11:45 Uhr</p>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Angebot erstellt von:</p>
+                    <p className="text-md text-strong mb-0">Karsten Wirler</p>
+                </td>
+                <td className="align-middle px-0 py-3 text-center">
+                    <a href="restaurant.html" className="btn btn-light rounded-pill text-sm">Angebot ausw&auml;hlen</a>
+                </td>
+            </tr>
+            <tr>
+                <td className="align-middle pl-0 pr-3 py-4">
+                    <div className="text-hand text-lg bg-gradient rounded-0 text-dark text-center px-1 py-2">McDonald's</div>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Bestellung m&ouml;glich:</p>
+                    <p className="text-md text-strong mb-0">Di, 11:15 &ndash; 11:45 Uhr</p>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Angebot erstellt von:</p>
+                    <p className="text-md text-strong mb-0">Adrian Brasin</p>
+                </td>
+                <td className="align-middle px-0 py-3 text-center">
+                    <a href="restaurant.html" className="btn btn-light rounded-pill text-sm">Angebot ausw&auml;hlen</a>
+                </td>
+            </tr>
+
+        </table>
+
+        <div className="border-top-light text-right pt-3">
+            <a href="create-offer.html" className="btn btn-link rounded-pill text-sm"><i className="fas fa-share mr-2"/>Angebot erstellen</a>
+        </div>
+    </div>
+}
+
+export function OfferListOld() {
+    return <div className="mx-0 mx-xl-5 my-4 px-5 py-3 bg-white border rounded-0">
+        <h6 className="text-dark text-strong px-0 py-3">Abgelaufene Angebote</h6>
+
+        <table className="table table-responsive-xl mb-0 text-center text-muted">
+            <tr>
+                <td className="align-middle pl-0 pr-3 py-4">
+                    <div className="text-hand text-lg bg-white rounded-0 text-dark text-center px-1 py-2">Pizzeria Venezia</div>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Bestellung m&ouml;glich:</p>
+                    <p className="text-md text-strong mb-0">Di, 11:15 &ndash; 11:45 Uhr</p>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Angebot erstellt von:</p>
+                    <p className="text-md text-strong mb-0">Angela B&ouml;hm</p>
+                </td>
+                <td className="align-middle px-0 py-3 text-center">
+                    <button className="btn btn-light rounded-pill text-sm" disabled>Angebot ausw&auml;hlen</button>
+                </td>
+            </tr>
+
+            <tr>
+                <td className="align-middle pl-0 pr-3 py-4">
+                    <div className="text-hand text-lg bg-white rounded-0 text-dark text-center px-1 py-2">Flying Pizza</div>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Bestellung m&ouml;glich:</p>
+                    <p className="text-md text-strong mb-0">Di, 11:15 &ndash; 11:45 Uhr</p>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Angebot erstellt von:</p>
+                    <p className="text-md text-strong mb-0">Sebastian M&uuml;ller</p>
+                </td>
+                <td className="align-middle px-0 py-3 text-center">
+                    <button className="btn btn-light rounded-pill text-sm" disabled>Angebot ausw&auml;hlen</button>
+                </td>
+            </tr>
+            <tr>
+                <td className="align-middle pl-0 pr-3 py-4">
+                    <div className="text-hand text-lg bg-white rounded-0 text-dark text-center px-1 py-2">Steffi's Imbiss</div>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Bestellung m&ouml;glich:</p>
+                    <p className="text-md text-strong mb-0">Di, 11:15 &ndash; 11:45 Uhr</p>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Angebot erstellt von:</p>
+                    <p className="text-md text-strong mb-0">Dominik Braun</p>
+                </td>
+                <td className="align-middle px-0 py-3 text-center">
+                    <button className="btn btn-light rounded-pill text-sm" disabled>Angebot ausw&auml;hlen</button>
+                </td>
+            </tr>
+            <tr>
+                <td className="align-middle pl-0 pr-3 py-4">
+                    <div className="text-hand text-lg bg-white rounded-0 text-dark text-center px-1 py-2">Flying Pizza</div>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Bestellung m&ouml;glich:</p>
+                    <p className="text-md text-strong mb-0">Di, 11:15 &ndash; 11:45 Uhr</p>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Angebot erstellt von:</p>
+                    <p className="text-md text-strong mb-0">Angela B&ouml;hm</p>
+                </td>
+                <td className="align-middle px-0 py-3 text-center">
+                    <button className="btn btn-light rounded-pill text-sm" disabled>Angebot ausw&auml;hlen</button>
+                </td>
+            </tr>
+            <tr>
+                <td className="align-middle pl-0 pr-3 py-4">
+                    <div className="text-hand text-lg bg-white rounded-0 text-dark text-center px-1 py-2">Steffi's Imbiss</div>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Bestellung m&ouml;glich:</p>
+                    <p className="text-md text-strong mb-0">Di, 11:15 &ndash; 11:45 Uhr</p>
+                </td>
+                <td className="align-middle py-3">
+                    <p className="text-sm mb-1">Angebot erstellt von:</p>
+                    <p className="text-md text-strong mb-0">Walter Seethaler</p>
+                </td>
+                <td className="align-middle px-0 py-3 text-center">
+                    <button className="btn btn-light rounded-pill text-sm" disabled>Angebot ausw&auml;hlen</button>
+                </td>
+            </tr>
+
+        </table>
+    </div>
+}
---
+++ ui-react/src/components/SidebarRight/SidebarRight.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 The FoodUnit Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from "react";
+
+export default function SidebarRight(props) {
+    return <div className="col-12 col-lg-3 col-xl-2 p-0 sidebar-right-lg bg-white border-left">
+        <h5 className="text-center border-bottom mb-0 px-2 py-4">Einkaufskorb</h5>
+        <p className="text-center text-strong mb-0 pt-5 pb-3 text-sm">Dein Einkaufskorb ist leer.</p>
+        <div className="px-4 py-4">
+            <div className="row m-0">
+                <div className="col-5 text-md text-strong text-muted px-0 py-2">Gesamt: 0.00 &euro;</div>
+                <div className="col-7 text-right p-0">
+                    <button className="btn btn-light rounded-pill text-xs" disabled><i className="fab fa-paypal mr-2"/>direkt zu PayPal</button>
+                </div>
+            </div>
+        </div>
+        <div className="px-4 pt-0 pb-4">
+            <button className="btn btn-success w-100 text-md rounded-0 text-strong py-2" disabled>Einkaufskorb speichern</button>
+        </div>
+    </div>
+}
